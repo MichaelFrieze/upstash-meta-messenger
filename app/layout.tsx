546cf3a7a0747be30264b59059112d57671676fa
Michael Frieze <michaelfrieze@icloud.com>
2023-01-27 18:50:46 -0500

    added session data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import "../styles/globals.scss";
-import Header from "../components/Header";
+import '../styles/globals.css';
+import Header from '../components/Header';
 
 export default function RootLayout({
   children,
